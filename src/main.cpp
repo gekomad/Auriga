861c5c91e2432cf0e960966b98c14fc8de49ec73
geko <geko@geko-HP-Compaq-6005-Pro-SFF-PC.seat.it>
2015-10-28 16:49:40 +0100

    .

--- src/main.cpp
+++ src/main.cpp
@@ -28,14 +28,47 @@
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-
-    cout << NAME << " - Distributed and collaborative perft framework by Giuseppe Cannella\n";
-    cout << "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n\n";
-#if UINTPTR_MAX != 0xffffffffffffffff
-    //32 bit
-    warn ("This version is 32-bit, manages results <= 2^64");
+void printHeader() {
+    cout << NAME << " - Distributed and collaborative Perft framework by Giuseppe Cannella\n";
+#if UINTPTR_MAX == 0xffffffffffffffff
+    cout << "version 64-bit ";
+#else
+    cout <<"This version is 32-bit, manages results <= 2^64\n";
 #endif
+
+    cout << "compiled " << __DATE__ << " with ";
+#if defined(__clang__)
+    cout << "Clang/LLVM " << __clang_major__ << "." << __clang_minor__ << "." << __clang_patchlevel__;
+#elif defined(__ICC) || defined(__INTEL_COMPILER)
+    cout << "Intel ICC "<<__VERSION__;
+#elif defined(__GNUC__) || defined(__GNUG__)
+    cout << "GNU GCC " << __VERSION__;
+#elif defined(__HP_cc) || defined(__HP_aCC)
+    cout << "Hewlett-Packard aC++" <<__HP_aCC;
+#elif defined(__IBMC__) || defined(__IBMCPP__)
+    cout << "IBM XL C++ <<"__IBMCPP__;
+#elif defined(_MSC_VER)
+    cout << "Microsoft Visual Studio. "<<_MSC_VER;
+#elif defined(__PGI)
+    cout << "Portland Group PGCC/PGCPP "<<__PGIC__;
+#elif defined(__SUNPRO_C) || defined(__SUNPRO_CC)
+    cout << "Oracle Solaris Studio "<<__SUNPRO_CC;
+#else
+    cout << "Unknown compiler";
+#endif
+    cout << "\nLicense GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n\n";
+#ifdef CLOP
+    cout << "CLOP ENABLED\n";
+#endif
+#ifdef DEBUG_MODE
+    cout << "DEBUG_MODE\n";
+#endif
+    cout << flush;
+}
+
+int main(int argc, char *argv[]) {
+    printHeader();
+
     GetOpt::parse(argc, argv);
     return 0;
 }
