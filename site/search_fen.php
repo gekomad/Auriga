b89ca15bf216dfee868e944481e96a20f86844d0
gekomad <info@cinnamon.com>
2015-10-31 23:11:11 +0100

    .

---
+++ site/search_fen.php
@@ -0,0 +1,83 @@
+<html><head>
+<meta name="robots" content="noindex">
+<title>Search Fen</title>
+<link rel="stylesheet"  href="css/css1.css" type="text/css"/>
+<link rel="stylesheet" href="css/layout.css" type="text/css" />
+
+<script type="text/javascript">
+
+function validate() {
+	return true;
+}
+</script>
+</head>
+  <?php include 'menu.php';?>   
+<body>
+
+<form method="post" onsubmit="return validate();" action="search_fen.php" >
+fen: <input name="fen" type="text" id="fen" placeholder="Fen string" />
+<input type="submit" class="submit btn outline" id="submit" value="Search" />
+</form>
+
+<section class="container">
+
+          <hgroup>
+<?php   
+$fen=$_POST['fen'];
+echo "<h1>Fen ".$fen."</h1> ";
+?>
+         </hgroup><div class="row"><section>
+              <h2>Depsloying code changes</h2>
+                <p>OpenShift uses the <a href="http://git-scm.com/">Git version control system</a>
+ for your source code, and grants you access to it via the Secure Shell 
+(SSH) protocol. In order to upload and download code to your application
+ you need to give us your <a href="https://developers.openshift.com/en/managing-remote-connection.html">public SSH key</a>. You can upload it within the web console or install the <a href="https://developers.openshift.com/en/managing-client-tools.html">RHC command line tool</a> and run <code>rhc setup</code> to generate and upload your key automatically.</p>
+               
+
+         <?php  
+
+if($fen!="") {
+	include 'mysql_connect.php';
+	$sql = "SELECT uuid_task FROM task_fens where fen ='".$fen."'";
+	$result = $conn->query($sql);
+
+	$uuid_task=$row["uuid_task"];
+	echo "<img src='http://webchess.freehostia.com/diag/chessdiag.php?fen=".$fen."&amp&size=large&amp&coord=yes&amp&cap=no&amp&stm=yes&amp&fb=no&amp&theme=classic&amp&color1=E3CEAA&amp&color2=635147&amp&color3=000000'  height='300' width='300'>";
+	echo "<br><br>fen: $fen<br><br>";
+	echo "depth: $deph<br>";
+	echo "tasks: $tasks<br>";
+	echo "creation_date: $creation_date<br>";
+	echo "<br>";
+
+	if ($result->num_rows > 0) {
+		echo "<table>";
+		echo "<tr>";
+		echo "<td><b>Task ID</b></td>" ;
+		echo "<td><b>Completed</b></td>" ;
+		echo "<td><b>Running</b></td>" ;
+	 	echo "</tr>";
+    	while($row = $result->fetch_assoc()) {
+			echo "<tr>";
+			echo '<td><a href="task.php?uuid_task='.$row["uuid_task"].'&uuid_perft='.$uuid_perft.'">'.$row["uuid_task"].'</a></td>';
+			$completed=($row["tot"] == 0 ?"no": "yes");
+			echo "<td>$completed</td>";
+			$running=$completed=="yes"?"":($row["partial"] == 0 ?"no": "yes");
+			echo "<td>$running</td>";
+			echo "</tr>";
+    	}
+		echo "</table>";
+	} else {
+    	echo "0 results";
+	}
+	$conn->close();	
+}
+
+
+
+?> 
+      </section>         
+        </div>
+</section>
+<?php include 'footer.html'; ?>    
+</body>
+</html>
