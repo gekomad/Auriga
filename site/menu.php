678e8731b66895209b6b298618065300ee1ca8f6
gekomad <info@cinnamon.com>
2015-10-31 13:35:09 +0100

    .

---
+++ site/menu.php
@@ -0,0 +1,151 @@
+<div class="wrapper">
+		<div class="sf_menu" id="topnav">
+			<ul class="nav">					
+						<li><a href="index.html">Home</a></li>					
+						<li><a href="/create_perft.php">Create Perft</a></li>
+					
+				
+					
+						<li><a href="#">Videos</a>
+						<ul>
+							
+								
+									<li><a href="/videos?v=Orcodituapse">Orcodituapse</a></li>
+								
+							
+								
+									<li><a href="/videos?v=Gorgonian">Gorgonian</a></li>
+								
+							
+								
+									<li><a href="https://www.youtube.com/watch?v=pMKwdghic5A&amp;feature=youtu.be&amp;noredirect=1">Ralph Möritz</a></li>
+								
+							
+						</ul>
+					
+				
+					
+						<li><a href="/ranking?rp=globals">Ranking</a></li>
+					
+				
+					
+						<li><a href="/docs">Docs</a></li>
+					
+				
+					
+						<li><a href="#">Version 9.07c</a>
+						<ul>
+							
+								
+									<li><a href="http://lucaschess.blogspot.com.es/2015/10/version-907a-problems-with-access-to.html">9.07a: problems with access to the Internet</a></li>
+								
+							
+								
+									<li><a href="http://lucaschess.blogspot.com.es/2015/09/version-907-16-gm-to-play-against-and.html">9.07: 16 GM to play against and major update of German translation</a></li>
+								
+							
+								
+									<li><a href="http://lucaschess.blogspot.com.es/2015/07/version-906-new-language-turkish.html">9.06: new language Turkish</a></li>
+								
+							
+								
+									<li><a href="http://lucaschess.blogspot.com.es/2015/06/version-905-some-bugs-fixed.html">9.05: some bugs fixed</a></li>
+								
+							
+								
+									<li><a href="http://lucaschess.blogspot.com.es/2015/05/version-904-playable-version-in-linux.html">9.04: a playable version in Linux</a></li>
+								
+							
+								
+									<li><a href="http://lucaschess.blogspot.com.es/2015/04/version-903-new-set-of-pieces-stauton.html">9.03a: new set of pieces, stauton 3D and new translation, Chinese simplified</a></li>
+								
+							
+								
+									<li><a href="http://lucaschess.blogspot.com.es/2015/03/version-902-some-bugs-fixed-and.html">9.02a: some bugs fixed and Etruscan, a new set of pieces</a></li>
+								
+							
+								
+									<li><a href="http://lucaschess.blogspot.com.es/2015/03/version-901-some-bugs-fixed.html">9.01: some bugs fixed</a></li>
+								
+							
+								
+									<li><a href="http://lucaschess.blogspot.com.es/2015/03/version-9-summary-of-changes.html">9.00: Summary of changes</a></li>
+								
+							
+						</ul>
+					
+				
+					
+						<li><a href="#">Downloads</a>
+						<ul>
+							
+								
+									<li><a href="https://bintray.com/artifact/download/lukasmonk/generic/LucasChessPortable907c.exe">Portable</a></li>
+								
+							
+								
+									<li><a href="https://bintray.com/artifact/download/lukasmonk/generic/InsLucasChess907c.exe">Installable</a></li>
+								
+							
+								
+									<li><a href="https://github.com/lukasmonk/lucaschess">GitHub</a></li>
+								
+							
+						</ul>
+					
+				
+					
+						<li><a href="#">Thanks to</a>
+						<ul>
+							
+								
+									<li><a href="thanks?clave=Contributors">Contributors</a></li>
+								
+							
+								
+									<li><a href="thanks?clave=Translators">Translators</a></li>
+								
+							
+								
+									<li><a href="thanks?clave=Images">Images</a></li>
+								
+							
+								
+									<li><a href="thanks?clave=Themes">Themes</a></li>
+								
+							
+								
+									<li><a href="thanks?clave=Pieces">Pieces</a></li>
+								
+							
+								
+									<li><a href="thanks?clave=Training">Training</a></li>
+								
+							
+								
+									<li><a href="thanks?clave=Engines-1">Engines/1</a></li>
+								
+							
+								
+									<li><a href="thanks?clave=Engines-2">Engines/2</a></li>
+								
+							
+								
+									<li><a href="thanks?clave=Games">Games</a></li>
+								
+							
+								
+									<li><a href="thanks?clave=Programming">Programming</a></li>
+								
+							
+								
+									<li><a href="thanks?clave=Dedicated">Dedicated to</a></li>
+								
+							
+						</ul>
+					
+				
+			</ul>
+			<div class="clear"></div>
+		</div>
+	</div>
