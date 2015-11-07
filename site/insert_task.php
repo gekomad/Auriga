67e61407483c72a7d81699f4a6d143c8ec0ef69c
gekomad <info@cinnamon.com>
2015-11-07 16:49:42 +0100

    .

--- site/insert_task.php
+++ site/insert_task.php
@@ -10,15 +10,21 @@ $fen=$_POST["fen"];
 $minutes=$_POST["minutes"];
 $depth=$_POST["depth"];
 
+
+$ip = $_SERVER['REMOTE_ADDR'];
+$details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
+$country=$details->country; 
+
+
 include 'mysql_connect.php';
 include 'updateStatistics.php';
 
 //insert row in tasks
-$sql = "INSERT INTO tasks (uuid_perft, uuid_task,heartbeat,tot,engine,author,fen,minutes,depth)VALUES ('$uuid_perft', '$uuid_task','$heartbeat','$tot','$engine','$author','$fen',$minutes,$depth)";
+$sql = "INSERT INTO tasks (uuid_perft, uuid_task,heartbeat,tot,engine,author,fen,minutes,depth)VALUES ('$uuid_perft', '$uuid_task','$heartbeat','$tot','$engine','$author','$fen',$minutes,$depth,$country)";
 $conn->query($sql);
 
 //update statistics in perft_tasks for uuid_task
-$sql = getUpdateTaskSQL($uuid_perft,$uuid_task);
+$sql = getUpdateTaskSQL($uuid_task);
 $conn->query($sql);
 
 //update statistics in perft for uuid_perft
