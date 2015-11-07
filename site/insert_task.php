8e302fd9b5bcd1e78e1c44bf72b20f0769bf66b7
gekomad <info@cinnamon.com>
2015-11-07 17:55:39 +0100

    .

--- site/insert_task.php
+++ site/insert_task.php
@@ -10,11 +10,14 @@ $fen=$_POST["fen"];
 $minutes=$_POST["minutes"];
 $depth=$_POST["depth"];
 
-
 $ip = $_SERVER['REMOTE_ADDR'];
+if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
+    $ip = array_pop(explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']));
+}
+
 $details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
 
-
+$country="";
 if(isset($details->country))$country=$details->country; 
 
 
@@ -22,7 +25,7 @@ include 'mysql_connect.php';
 include 'updateStatistics.php';
 
 //insert row in tasks
-$sql = "INSERT INTO tasks (uuid_perft, uuid_task,heartbeat,tot,engine,author,fen,minutes,depth,country)VALUES ('$uuid_perft', '$uuid_task','$heartbeat','$tot','$engine','$author','$fen',$minutes,$depth,$country)";
+$sql = "INSERT INTO tasks (uuid_perft, uuid_task,heartbeat,tot,engine,author,fen,minutes,depth,country)VALUES ('$uuid_perft', '$uuid_task','$heartbeat','$tot','$engine','$author','$fen',$minutes,$depth,'$country')";
 $conn->query($sql);
 
 //update statistics in perft_tasks for uuid_task
