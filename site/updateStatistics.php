51a45765410ddb23159b0eb54acfec4011d813af
gekomad <info@cinnamon.com>
2015-11-07 12:24:13 +0100

    .

--- site/updateStatistics.php
+++ site/updateStatistics.php
@@ -1,34 +1,34 @@
 <?php
 
-function getUpdateTaskSQL($uuid_perft,$uuid_task){
+function getUpdateTaskSQL($uuid_task){
 return "update perft_tasks pt 
 join 
 	(	
 	select pt.uuid_perft, pt.uuid_task,  
 		(select count(distinct engine)from tasks tt2 where tt2.uuid_task=t.uuid_task 
-		  and tt2.uuid_task='".$uuid_perft."'
+		  and tt2.uuid_task='".$uuid_task."'
 		  group by tt2.uuid_task,tt2.uuid_perft )n_engine,  	
-		  (select sum(d.tot) from ( select avg(tot*(select count(1) from task_fens tf2 where tf2.uuid_task='".$uuid_perft."' and fen =tt2.fen
+		  (select sum(d.tot) from ( select avg(tot*(select count(1) from task_fens tf2 where tf2.uuid_task='".$uuid_task."' and fen =tt2.fen
 )) tot from tasks tt2 where
 		   ifnull(tt2.tot,-1)!=-1 
-		  and tt2.uuid_task='".$uuid_perft."'
+		  and tt2.uuid_task='".$uuid_task."'
 		  group by fen )d)tot,  
      	  (select sum(minutes)from tasks tt2 where tt2.uuid_task=t.uuid_task 
-		  and tt2.uuid_task='".$uuid_perft."'
+		  and tt2.uuid_task='".$uuid_task."'
 		  and tt2.tot is not null
 		  group by tt2.uuid_task,tt2.uuid_perft )minutes,  
 	ifnull(floor ((select count(fen) from task_fens tf2 where id in(
 	select distinct id from task_fens tf2 where tf2.uuid_task =t.uuid_task  and tf2.fen in (
 select distinct f.fen from task_fens f
-where f.uuid_task='".$uuid_perft."' )))/fens*100),0) perc_completed , 
+where f.uuid_task='".$uuid_task."' )))/fens*100),0) perc_completed , 
 	max(t.creation_date)creation_date from perft_tasks pt  
 	left join tasks t  
 	on  t.uuid_task=pt.uuid_task  
-	where pt.uuid_task ='".$uuid_perft."' 
+	where pt.uuid_task ='".$uuid_task."' 
 	group by pt.uuid_perft,pt.uuid_task  	
 	)t 
 on t.uuid_perft=pt.uuid_perft and t.uuid_task=pt.uuid_task 
-and pt.uuid_task ='".$uuid_perft."' 
+and pt.uuid_task ='".$uuid_task."' 
 set pt.engine =t.n_engine, pt.perc_completed = t.perc_completed,
 pt.creation_date= t.creation_date,pt.tot= t.tot,
 pt.hours= t.minutes/60";
