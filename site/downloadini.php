d1e3404b98acebce7327089a17fbd9cf20706f09
gekomad <info@cinnamon.com>
2015-11-08 14:26:23 +0100

    .

--- site/downloadini.php
+++ site/downloadini.php
@@ -2,4 +2,22 @@
 
 header('Content-type: application/x-gzip');
 include 'set_variable.php';	
-echo "XXX".filesize($auriga_root."/data/".$_GET['id']."/".$_GET['id'].".ini.gz")."XXX".file_get_contents($auriga_root."/data/".$_GET['id']."/".$_GET['id'].".ini.gz");?>
+$uuid_perft=$_GET['id'];
+$uuid_task="";
+if($uuid_perft==""){
+	include 'mysql_connect.php';
+	$sql="SELECT uuid_perft,uuid_task FROM perft_tasks where tot is null ORDER BY RAND() LIMIT 0,1";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+		$row = $result->fetch_assoc();
+		$uuid_perft=$row['uuid_perft'];
+		$uuid_task=$row['uuid_task'];
+	}	
+	$conn->close();
+}
+if($uuid_perft==""){
+	echo "X";
+	exit(0);
+}
+$filename=$auriga_root."/data/".$uuid_perft."/".$uuid_perft.".ini.gz";
+echo "XXX".$uuid_perft."XXX".$uuid_task."XXX".filesize($filename)."XXX".file_get_contents($filename);?>
