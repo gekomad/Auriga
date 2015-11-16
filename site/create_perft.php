1a6403e551cde047f752a6a258b8f5cdc16a1e07
geko <geko@geko-HP-Compaq-6005-Pro-SFF-PC.seat.it>
2015-11-16 14:01:26 +0100

    .

--- site/create_perft.php
+++ site/create_perft.php
@@ -13,27 +13,19 @@ header("Expires: 0"); // Proxies.
     <script src="js/cinnamon_engine_1_2b.js"></script>
    <script>
 	function calculateTasks(){
-		gettasks = Module.cwrap('getntasks', 'number', ['string','number','number']);//TODO solo una volta
+		document.getElementById("tree").innerHTML ="";
+		if(validate_single_fen()==false)return;		
+		fen=document.getElementById("fen").value;
+		var rd=1;
+		gettasks = Module.cwrap('getntasks', 'number', ['string','number']);//TODO solo una volta
 		
-
-		if(validate_single_fen()==false)return;
-		reducedDepth =document.getElementById('reducedepth');
-		if(!isNumeric(reducedDepth.value)|| (reducedDepth.value<0||reducedDepth.value>10)){	
-			document.getElementById("error_reducedepth").className = 'nohidden';
-			reducedDepth.style.backgroundColor = "red";
-			return;
-		}else 	{
-			document.getElementById("error_reducedepth").className = 'hidden';
-			reducedDepth.style.backgroundColor = "initial";
+		while(res<1000)
+		{
+			res = gettasks(fen,rd);			
+			document.getElementById("tree").innerHTML ="min tasks: "+res+" reduce depth: "+rd+"\n";
+			rd++;
 		}
-
 		
-alert(fen+ " "+reducedDepth.value);
-		rd=0;
-		var res = gettasks(fen,rd);
-		ciclo fino a 1000 tasks
-		document.getElementById('reducetasks_real').innerHTML = "depth reduce to "+reduce;else document.getElementById('reducetasks_real').innerHTML = "";
-
 }
  </script> 
     <?php include 'captcha.php';	?>
@@ -49,11 +41,18 @@ alert(fen+ " "+reducedDepth.value);
     </hgroup>   
     <div class="row">
         <section>
+	<aside>
+<div class="row">
+<textarea id="tree" type="text" style=" border: none; background-color: Lavender ;resize: none ; overflow:hidden;"></textarea>
+
+</div>
+</aside>
             <form method="post" onsubmit="return validate();" action="generate_master_ini.php" name="perftform" id="perftform" class="form form-stacked c-form">
                 fen:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input name="fen" type="text" id="fen" placeholder="Fen string"/>
-                <small id="error_fen" class="hidden">fen format is wrong [fen validator: <a href="https://github.com/jhlywa/chess.js">https://github.com/jhlywa/chess.js</a>]</small>
-  				<br>
-				insert tasks numbers or calculate it on reduced depth: <input name="tasks" type="text" id="reducedepth" placeholder="reduced depth"/><input id="calculatebutton" type="button" value="clickme" onclick="calculateTasks();" /> <small id="reducetasks_real"></small><small id="error_reducedepth" class="hidden">reduce depth is a number between 1 and 10</small> <br>
+				<input id="calculatebutton" type="button" value="Calculate Tasks" onclick="calculateTasks();" /> 	
+                <br><small id="error_fen" class="hidden">fen format is wrong [fen validator: <a href="https://github.com/jhlywa/chess.js">https://github.com/jhlywa/chess.js</a>]</small>
+  				
+				<br>
 			
 				<br>tasks:&nbsp;&nbsp;<input name="tasks" type="text" id="tasks" placeholder="tasks number"/>
                 <small id="error_tasks" class="hidden">tasks is a number between 1 and 1000</small>
@@ -61,7 +60,6 @@ alert(fen+ " "+reducedDepth.value);
                 <br>
                 depth:&nbsp;<input name="depth" type="text" id="depth" placeholder="depth"/>
                 <small id="error_depth" class="hidden">depth is a number between 2 and 100</small>
-              
                 
 
                 <br><br>
@@ -72,6 +70,7 @@ alert(fen+ " "+reducedDepth.value);
                 <br><br><input type="submit" class="submit btn outline" id="submit" value="Create perft"/>
             </form>
         </section>
+
     </div>
 
 
