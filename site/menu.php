e463f97f10d634e3552289fbb978ae84b9486ad8
gekomad <info@cinnamon.com>
2015-11-18 22:11:08 +0100

    .

--- site/menu.php
+++ site/menu.php
@@ -3,7 +3,7 @@
 		<div class="sf_menu" id="topnav">
 			<ul class="nav">					
 						<li><a href="index.php">Home</a></li>					
-						<li><a href="/allperft.php">All perfts</a></li>					
+						<li><a href="/allperft.php">All Perfts</a></li>					
 						<li><a href="/create_perft.php">Create a Perft</a></li>											
 						<ul>								
 									<li><a href="/videos?v=Orcodituapse">How to works Auriga</a></li>
