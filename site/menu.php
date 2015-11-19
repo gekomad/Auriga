8a3cb4dfcf3fbd548d6e4a02a04ce37ce2b8c75b
gekomad <info@cinnamon.com>
2015-11-19 22:15:03 +0100

    .

--- site/menu.php
+++ site/menu.php
@@ -2,21 +2,13 @@
 <div class="wrapper">
 		<div class="sf_menu" id="topnav">
 			<ul class="nav">					
-						<li><a href="index.php">Home</a></li>					
+						<li><a href="/index.php">Home</a></li>					
 						<li><a href="/allperft.php">All Perfts</a></li>					
-						<li><a href="/create_perft.php">Create a Perft</a></li>											
-						<ul>								
-									<li><a href="/videos?v=Orcodituapse">How to works Auriga</a></li>
-									<li><a href="https://www.youtube.com/watch?v=pMKwdghic5A&amp;feature=youtu.be&amp;noredirect=1">Worker</a></li>								
-									<li><a href="/videos?v=Orcodituapse">Create a Perft</a></li>								
-									<li><a href="/videos?v=Gorgonian">Tasks</a></li>
-									<li><a href="https://www.youtube.com/watch?v=pMKwdghic5A&amp;feature=youtu.be&amp;noredirect=1">Working in your intranet</a></li>
-						</ul>
-
+						<li><a href="/create_perft.php">Create a Perft</a></li>																	
 						<li><a href="#">Fen</a>										
 						<ul>								
-								<li><a href="search_fen.php">Search</a></li>
-								<li><a href="anomaly.php">Anomaly</a></li>
+								<li><a href="/search_fen.php">Search</a></li>
+								<li><a href="/anomaly.php">Anomaly</a></li>
 						</ul></li>	
 
 						
