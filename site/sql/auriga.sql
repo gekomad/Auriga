328f48ed337cef44c143bc8a3f3a6a4f0fce4d25
gekomad <info@cinnamon.com>
2015-11-20 18:30:27 +0100

    .

--- site/sql/auriga.sql
+++ site/sql/auriga.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.44, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.1.73, for redhat-linux-gnu (x86_64)
 --
--- Host: localhost    Database: auriga
+-- Host: 127.12.95.2    Database: auriga
 -- ------------------------------------------------------
--- Server version	5.5.44-0ubuntu0.14.04.1
+-- Server version	5.5.45
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -14,6 +14,30 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `errors`
+--
+
+DROP TABLE IF EXISTS `errors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `errors` (
+  `id` int(10) unsigned NOT NULL,
+  `uuid_perft` char(36) COLLATE utf8_bin NOT NULL,
+  `uuid_task` char(36) COLLATE utf8_bin NOT NULL,
+  `fen` char(100) COLLATE utf8_bin NOT NULL,
+  `depth` int(11) DEFAULT NULL,
+  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `heartbeat` tinyint(1) DEFAULT NULL,
+  `tot` bigint(20) unsigned DEFAULT NULL,
+  `engine` varchar(30) COLLATE utf8_bin DEFAULT NULL,
+  `author` varchar(30) COLLATE utf8_bin DEFAULT NULL,
+  `minutes` int(11) DEFAULT NULL,
+  `country` char(2) COLLATE utf8_bin DEFAULT NULL,
+  `anomaly` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `ip_country_timezone`
@@ -45,9 +69,9 @@ CREATE TABLE `perft` (
   `creation_date` timestamp NULL DEFAULT NULL,
   `tot` varchar(40) COLLATE utf8_bin DEFAULT NULL,
   `perc_completed` tinyint(4) DEFAULT NULL,
-  `hours` int(10) unsigned DEFAULT NULL,
+  `minutes` int(11) DEFAULT NULL,
   `engines` int(11) DEFAULT NULL,
-  `confermated` tinyint(1) DEFAULT NULL,
+  `confirmed` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`uuid_perft`),
   UNIQUE KEY `fen` (`fen`,`depth`,`tasks`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
@@ -83,10 +107,23 @@ CREATE TABLE `perft_tasks` (
   `perc_completed` tinyint(4) DEFAULT NULL,
   `creation_date` timestamp NULL DEFAULT NULL,
   `tot` bigint(20) unsigned DEFAULT NULL,
-  `hours` int(10) unsigned DEFAULT NULL,
+  `minutes` int(11) DEFAULT NULL,
   PRIMARY KEY (`uuid_perft`,`uuid_task`),
   UNIQUE KEY `uuid_task` (`uuid_task`),
   CONSTRAINT `ptiaisks_i0f3_2` FOREIGN KEY (`uuid_perft`) REFERENCES `perft` (`uuid_perft`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `personal_uuid`
+--
+
+DROP TABLE IF EXISTS `personal_uuid`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `personal_uuid` (
+  `uuid` char(36) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -106,7 +143,7 @@ CREATE TABLE `task_fens` (
   KEY `task_fs_fen_IDX` (`fen`),
   KEY `ptasksds_bf3_2` (`uuid_task`),
   CONSTRAINT `iptaxdds_bf3_2` FOREIGN KEY (`uuid_task`) REFERENCES `perft_tasks` (`uuid_task`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2445 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=2440 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -130,11 +167,14 @@ CREATE TABLE `tasks` (
   `minutes` int(11) DEFAULT NULL,
   `country` char(2) COLLATE utf8_bin DEFAULT NULL,
   `anomaly` int(11) DEFAULT NULL,
+  `personal_uuid` char(36) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`),
   KEY `uuid_task` (`uuid_task`),
   KEY `tadkds_ibifk_1` (`uuid_perft`),
-  CONSTRAINT `tadkids_ibfk_1` FOREIGN KEY (`uuid_perft`) REFERENCES `perft` (`uuid_perft`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=2544 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+  KEY `tadbfk_1` (`personal_uuid`),
+  CONSTRAINT `tadbfk_1` FOREIGN KEY (`personal_uuid`) REFERENCES `personal_uuid` (`uuid`),
+  CONSTRAINT `tadkdds_ibfk_1` FOREIGN KEY (`uuid_perft`) REFERENCES `perft` (`uuid_perft`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6410 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -146,4 +186,4 @@ CREATE TABLE `tasks` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-11-15 11:15:53
+-- Dump completed on 2015-11-20 12:29:38
