4858a3f3b29260324d95e6d668d7235be27c16cd
geko <geko@geko-HP-Compaq-6005-Pro-SFF-PC.seat.it>
2015-11-24 15:08:06 +0100

    .

--- web_site/download.php
+++ web_site/download.php
@@ -12,16 +12,16 @@
 <div class="container2">
 <section class="container">
           <hgroup>
-            <h1>Download Auriga </h1> 
+            <h1>Download Auriga</h1> 
           </hgroup>
 
         <div class="row">
           <section class="col-xs-12 col-sm-6 col-md-6">			
-			<p><a href="https://chsfsft"><img src="/img/os/WIN.png" height="50" width="50" />Windows</a> </p>
-			<p><a href="https://chsfsft"><img src="/img/os/LINUX.png" height="50" width="50" />Linux</a> </p>
-			<p><a href="https://chsfsft"><img src="/img/os/APPLE.png" height="50" width="50" />Mac</a> </p>
-			<p><a href="https://chsfsft"><img src="/img/os/ARM.png" height="50" width="50" />Arm</a> </p>
-     		<p><a href="https://chsfsft"><img src="/img/GITHUB.png" height="42" width="50" />Source</a> </p>         
+			<p><a href="/bin/auriga_windows_1.0.zip"><img src="/img/os/WIN.png" height="50" width="50" />Windows</a> </p>
+			<p><a href="/bin/auriga_linux_1.0.zip"><img src="/img/os/LINUX.png" height="50" width="50" />Linux</a> </p>
+			<p><a href="/bin/auriga_osx_1.0.zip"><img src="/img/os/APPLE.png" height="50" width="50" />Mac</a> </p>
+			<p><a href="/bin/auriga_arm_1.0.zip"><img src="/img/os/ARM.png" height="50" width="50" />Arm</a> </p>
+     		<p><a href="https://github.com/gekomad/Auriga"><img src="/img/GITHUB.png" height="42" width="50" />Source</a> </p>         
           </section>        
 	   </div>
 
