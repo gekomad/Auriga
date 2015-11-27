0d06a204f63d22a807c0e6ffdfd742676882d325
gekomad <info@cinnamon.com>
2015-11-27 20:43:23 +0100

    .

--- web_site/download.php
+++ web_site/download.php
@@ -19,7 +19,8 @@
         <div class="row">
           <section class="col-xs-12 col-sm-6 col-md-6">			
 			<p><a href="/bin/auriga_windows_1.0.zip"><img src="/img/os/WIN.png" height="50" width="50" />Windows</a> </p>
-			<p><a href="/bin/auriga_linux_1.0.zip"><img src="/img/os/LINUX.png" height="50" width="50" />Linux</a> </p>
+			<p><a href="/bin/auriga64_linux_1.0.zip"><img src="/img/os/LINUX.png" height="50" width="50" />Linux 64 bit</a> </p>
+			<p><a href="/bin/auriga32_linux_1.0.zip"><img src="/img/os/LINUX.png" height="50" width="50" />Linux 32 bit</a> </p>
 			<p><a href="/bin/auriga_osx_1.0.zip"><img src="/img/os/APPLE.png" height="50" width="50" />Mac</a> </p>
 			<p><a href="/bin/auriga_arm_1.0.zip"><img src="/img/os/ARM.png" height="50" width="50" />Arm</a> </p>
      		<p><a href="https://github.com/gekomad/Auriga"><img src="/img/GITHUB.png" height="42" width="50" />Source</a> </p>         
